f720d8350778c47d9acf18abebd8a4eee76a952c
Ronick Rodrigues <86311027+RonickRodrigues@users.noreply.github.com>
2021-11-25 17:03:39 -0300

    add.test3

--- doWhile.java
+++ doWhile.java
@@ -1,23 +1,37 @@
+import java.util.Locale;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class doWhile {
     
     public static void main(String[] args) {
 
+        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        double temp = 0;
-        char condicao = ' ';
+        /*double temp = 0.0;
+        String condicao = "";
 
         do {
             System.out.println("Digite a temperatura em Celsius: ");
             temp = sc.nextDouble();
             temp = ((temp * 9) / 5) + 32;
-            System.out.printf("Equivalente em Fahrenheit: %.1f", temp);
+            System.out.printf("Equivalente em Fahrenheit: %.1f%n", temp);
             System.out.println("Deseja repetir (s/n)?");
-            sc.next().charAt(0);
+            sc.next();
 
-        } while (condicao != 's');
+        } while (condicao == "s");*/
+
+        char resp;
+        do {
+            System.out.print("Digite a temperatura em Celsius: ");
+            double C = sc.nextDouble();
+            double F = 9.0 * C / 5.0 + 32.0;
+            System.out.printf("Equivalente em Fahrenheit: %.1f%n", F);
+            System.out.print("Deseja repetir (s/n)?");
+            resp = sc.next().charAt(0);
+
+        } while (resp != 'n');
 
         sc.close();
     }
--- forTest.java
+++ forTest.java
@@ -4,7 +4,7 @@ public class forTest {
     
     public static void main(String[] args) {
 
-        /*Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         int N = sc.nextInt();
         int soma = 0;
@@ -16,7 +16,7 @@ public class forTest {
 
         System.out.println(soma);
 
-        sc.close();*/
+        sc.close();
 
         for( int i =0; i < 5; i++) {
             System.out.println("Valor de i: " + i);
