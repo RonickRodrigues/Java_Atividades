6f4595ddfa4c395aa2c0df18ad2c0c1fd1d9bccf
Ronick Rodrigues <86311027+RonickRodrigues@users.noreply.github.com>
2021-11-25 15:30:08 -0300

    add_01

---
+++ Repeticao/src/App.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+
+        while (x != 0) {
+            x = sc.nextInt();
+            
+        }
+
+        sc.close();
+    }
+}
