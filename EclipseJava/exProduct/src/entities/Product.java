69a740632aea5cf8920943aabe82ae3018ac8c4f
Ronick Rodrigues <86311027+RonickRodrigues@users.noreply.github.com>
2021-11-26 15:05:39 -0300

    add05

---
+++ EclipseJava/exProduct/src/entities/Product.java
@@ -0,0 +1,16 @@
+package entities;
+
+public class Product {
+	
+	public String Name;
+	public double Price;
+	public int Quantity;
+	
+	public static double TotalValueInStock() {
+		
+	}
+	
+	public static void AddProducts() {
+		
+	}
+}
